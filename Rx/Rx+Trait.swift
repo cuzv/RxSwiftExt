55c16794a1a0a64a5c99cf9729006d7eaeb14662
Shaw <cuzval@gmail.com>
2019-01-27 14:14:17 +0800

    Update Rx.

--- Rx/Rx+Trait.swift
+++ Rx/Rx+Trait.swift
@@ -1,9 +1,8 @@
-//
-//  Created by Shaw.
-//
-
 import Foundation
 import RxSwift
+import RxCocoa
+
+// MARK: - Schedluer
 
 public enum RxScheduler {
     case main
@@ -30,3 +29,44 @@ public extension ObservableType {
         return subscribeOn(scheduler.toImmediateScheduler())
     }
 }
+
+// MARK: - Operator
+
+public extension ObservableType {
+    public func void() -> Observable<Void> {
+        return asObservable().map({ _ in })
+    }
+    
+    public func ignoreNil<R>() -> Observable<R> where E == R? {
+        return self.asObservable().flatMap { $0.map(Observable.just) ?? .empty() }
+    }
+}
+
+// MARK: - Binder
+
+public extension Reactive where Base: AnyObject {
+    public func makeBinder(_ action: @escaping (Base) -> () -> ()) -> Binder<Void> {
+        return Binder(base) { (base, _) in
+            action(base)()
+        }
+    }
+    
+    public func makeBinder<Value>(_ action: @escaping (Base) -> (Value) -> ()) -> Binder<Value> {
+        return Binder(base) { (base, value) in
+            action(base)(value)
+        }
+    }
+    
+    public func makeBinder<V1, V2>(_ action: @escaping (Base) -> (V1, V2) -> ()) -> Binder<(V1, V2)> {
+        return Binder(base) { (base, value) in
+            action(base)(value.0, value.1)
+        }
+    }
+    
+    public func makeBinder<V1, V2, V3>(_ action: @escaping (Base) -> (V1, V2, V3) -> ()) -> Binder<(V1, V2, V3)> {
+        return Binder(base) { (base, value) in
+            action(base)(value.0, value.1, value.2)
+        }
+    }
+}
+
