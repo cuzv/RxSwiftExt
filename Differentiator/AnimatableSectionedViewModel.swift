d8d98af36e433891cca914fd9618044cc03d9f9c
Shaw <cuzval@gmail.com>
2019-03-08 12:39:35 +0800

    Update name.

--- Differentiator/AnimatableSectionedViewModel.swift
+++ Differentiator/AnimatableSectionedViewModel.swift
@@ -8,24 +8,24 @@ public protocol AnimatableSectionedViewModel {
     
     func numberOfSections() -> Int
     func numberOfItems(inSection section: Int) -> Int
-    func model(inSection section: Int) -> ModelOfAnimatableSection
+    func model(forSectionAt section: Int) -> ModelOfAnimatableSection
 }
 
 public extension AnimatableSectionedViewModel {
-    public func model(atIndexPath indexPath: IndexPath) -> Item {
-        return model(inSection: indexPath.section).items[indexPath.item]
+    public func model(forItemAt indexPath: IndexPath) -> Item {
+        return model(forSectionAt: indexPath.section).items[indexPath.item]
     }
     
     public func allModels(_ isIncluded: (Item) -> Bool) -> [Item] {
         return (0 ..< numberOfSections())
-            .map({ model(inSection: $0) })
+            .map({ model(forSectionAt: $0) })
             .flatMap({ $0.items })
             .filter(isIncluded)
     }
     
     public func indexPath(of model: Item) -> IndexPath? {
         for section in 0 ..< numberOfSections() {
-            if let item = self.model(inSection: section).items.firstIndex(where: { $0 == model }) {
+            if let item = self.model(forSectionAt: section).items.firstIndex(where: { $0 == model }) {
                 return IndexPath(item: item, section: section)
             }
         }
--- Differentiator/SectionedViewModel.swift
+++ Differentiator/SectionedViewModel.swift
@@ -8,17 +8,17 @@ public protocol SectionedViewModel {
     
     func numberOfSections() -> Int
     func numberOfItems(inSection section: Int) -> Int
-    func model(inSection section: Int) -> ModelOfSection
+    func model(forSectionAt section: Int) -> ModelOfSection
 }
 
 public extension SectionedViewModel {
-    public func model(atIndexPath indexPath: IndexPath) -> Item {
-        return model(inSection: indexPath.section).items[indexPath.item]
+    public func model(forItemAt indexPath: IndexPath) -> Item {
+        return model(forSectionAt: indexPath.section).items[indexPath.item]
     }
     
     public func allModels(_ isIncluded: (Item) -> Bool) -> [Item] {
         return (0 ..< numberOfSections())
-            .map({ model(inSection: $0) })
+            .map({ model(forSectionAt: $0) })
             .flatMap({ $0.items })
             .filter(isIncluded)
     }
@@ -27,7 +27,7 @@ public extension SectionedViewModel {
 public extension SectionedViewModel where Item: Equatable {
     public func indexPath(of model: ModelOfSection.Item) -> IndexPath? {
         for section in 0 ..< numberOfSections() {
-            if let item = self.model(inSection: section).items.firstIndex(where: { $0 == model }) {
+            if let item = self.model(forSectionAt: section).items.firstIndex(where: { $0 == model }) {
                 return IndexPath(item: item, section: section)
             }
         }
