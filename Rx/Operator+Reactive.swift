b7e244e3a8de5e8d7dd95d32010ed075362db5ba
Shaw <cuzval@gmail.com>
2019-07-06 21:49:43 +0800

    Add & update.

--- Rx/Operator+Reactive.swift
+++ Rx/Operator+Reactive.swift
@@ -5,66 +5,92 @@ extension ObservableType {
         return replaceWith(())
     }
 
-    public func ignoreNil<Revision>() -> Observable<Revision> where Element == Revision? {
+    public func replaceWith<NewElement>(_ element: NewElement) -> Observable<NewElement> {
+        return map { _ in element }
+    }
+
+    public func unwrap<Wrapped>() -> Observable<Wrapped> where Element == Wrapped? {
         return flatMap(Observable.from(optional:))
     }
 
-    public func ignoreErrorAndNil<Revision>() -> Observable<Revision> where Element == Revision? {
-        return catchErrorJustReturn(nil).flatMap(Observable.from(optional:))
-    }
-
-    public func map<Revision>(_ keyPath: KeyPath<Element, Revision>) -> Observable<Revision> {
+    public func map<Result>(_ keyPath: KeyPath<Element, Result>) -> Observable<Result> {
         return map { $0[keyPath: keyPath] }
     }
 
-    public func compactMap<Revision>(_ keyPath: KeyPath<Element, Revision?>) -> Observable<Revision> {
+    public func compactMap<Result>(_ keyPath: KeyPath<Element, Result?>) -> Observable<Result> {
         return compactMap { $0[keyPath: keyPath] }
     }
 
-    public func `as`<Revision>(_ transformedType: Revision.Type) -> Observable<Revision?> {
-        return map { $0 as? Revision }
+    public func `as`<Transformed>(_ transformedType: Transformed.Type) -> Observable<Transformed?> {
+        return map { $0 as? Transformed }
     }
 
-    public func compactAs<Revision>(_ transformedType: Revision.Type) -> Observable<Revision> {
-        return compactMap { $0 as? Revision }
+    public func of<Transformed>(_ transformedType: Transformed.Type) -> Observable<Transformed> {
+        return compactMap { $0 as? Transformed }
     }
 
     public func filter(_ keyPath: KeyPath<Element, Bool>) -> Observable<Element> {
         return filter { $0[keyPath: keyPath] }
     }
 
-    public func filterReversed(_ keyPath: KeyPath<Element, Bool>) -> Observable<Element> {
+    public func ignore(_ keyPath: KeyPath<Element, Bool>) -> Observable<Element> {
         return filter { !$0[keyPath: keyPath] }
     }
 
-    public func replaceWith<NewElement>(_ element: NewElement) -> Observable<NewElement> {
-        return map({ _ in element })
+    public func ignore(_ predicate: @escaping (Element) throws -> Bool) -> Observable<Element> {
+        return filter { try !predicate($0) }
+    }
+
+    public func catchErrorJustComplete() -> Observable<Element> {
+        return catchError { _ in .empty() }
+    }
+}
+
+extension ObservableType where Element: Equatable {
+    public func filter(_ valuesToFilter: Element...) -> Observable<Element> {
+        return filter { valuesToFilter.contains($0) }
+    }
+
+    public func filter<Sequence: Swift.Sequence>(_ valuesToFilter: Sequence) -> Observable<Element> where Sequence.Element == Element {
+        return filter { valuesToFilter.contains($0) }
+    }
+
+    public func ignore(_ valuesToIgnore: Element...) -> Observable<Element> {
+        return filter { !valuesToIgnore.contains($0) }
+    }
+
+    public func ignore<Sequence: Swift.Sequence>(_ valuesToIgnore: Sequence) -> Observable<Element> where Sequence.Element == Element {
+        return filter { !valuesToIgnore.contains($0) }
     }
 }
 
 extension ObservableType where Element == String? {
     public func ignoreEmpty() -> Observable<String> {
-        return ignoreNil().ignoreEmpty()
+        return unwrap().ignoreEmpty()
     }
 }
 
 extension ObservableType where Element == Bool {
-    public func reversed() -> Observable<Bool> {
-        return map({ !$0 })
+    public func not() -> Observable<Bool> {
+        return map(!)
     }
 
-    public func ignoreTrue() -> Observable<Bool> {
-        return filter({ !$0 })
+    public func takeFalse() -> Observable<Bool> {
+        return filter(!)
     }
 
-    public func ignoreFalse() -> Observable<Bool> {
-        return filter({ $0 })
+    public func takeTrue() -> Observable<Bool> {
+        return filter { $0 }
     }
 }
 
 extension ObservableType where Element: Collection {
     public func ignoreEmpty() -> Observable<Element> {
-        return filterReversed(\.isEmpty)
+        return ignore(\.isEmpty)
+    }
+
+    public func mapElements<Transformed>(_ transform: @escaping (Element.Element) throws -> Transformed) -> Observable<[Transformed]> {
+        return map { try $0.map(transform) }
     }
 }
 
@@ -85,5 +111,11 @@ extension ObservableType where Element: ResultConvertible {
 
     public func errors() -> Observable<Element.Failure> {
         return compactMap(\.result.failure)
+    }
+}
+
+extension Observable {
+    public func merge(_ others: Observable<Element>...) -> Observable<Element> {
+        return Observable.merge([self] + others)
     }
 }
